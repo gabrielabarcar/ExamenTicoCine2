1ae84648fde2a5bff1b9df49faa9a183a9d2b6cd
Gabriel Abarca <gabrielabarcar@hotmail.com>
2020-08-10 23:03:30 -0600

    Version1

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php   require_once './Vistas/include/header.php'; ?>
 <!DOCTYPE html>
-<html>
+<html lang="es">
     <head>
-        <meta charset="UTF-8">
+        <meta charset="utf-8">
         <title>Cine</title>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body>
         <center>      
